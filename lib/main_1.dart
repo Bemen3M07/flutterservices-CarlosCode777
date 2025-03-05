0112c31d1983f5a4ef91b348b9a21a8a0e9504f3
CarlosCode777 <casico23@bemen3.cat>
2025-03-05 19:10:41 +0000

    class

--- lib/main_1.dart
+++ lib/main_1.dart
@@ -1,16 +1,21 @@
 class CarsMode{
-CarsModel([
-  required this.Id,
-  required this.year,
-  required this.make,
-  required this.carsmodel,
-  required this.type
-]);
+  CarsModel([
+    required this.Id,
+    required this.year,
+    required this.make,
+    required this.carsmodel,
+    required this.type
+  ]);
 
-final int id;
-final int year;
-final String make;
-final String model;
-final String type;
+  final int id;
+  final int year;
+  final String make;
+  final String model;
+  final String type;
+}
 
+class CarHttpService{
+  getCars(){
+    http.get[URL]
+  }
 }
