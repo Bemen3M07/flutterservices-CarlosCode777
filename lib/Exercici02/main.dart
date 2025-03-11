9bf23072c8b27d79da1e2d30a8bfda07fefdbdcd
CarlosCode777 <casico23@bemen3.cat>
2025-03-11 16:20:46 +0100

    main y homedart

--- lib/Exercici02/main.dart
+++ lib/Exercici02/main.dart
@@ -1,3 +1,4 @@
+import 'package:empty/Exercici02/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+
+      title: 'Uso Provider',
+
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
       ),
+
+      home: HomeScreen(),
     );
   }
 }
